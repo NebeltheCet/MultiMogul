9ce87c870859535f53bb9a2c36227577c13916b0
NebeltheCet <109384077+NebeltheCet@users.noreply.github.com>
2025-12-27 21:44:17 +0100

    made new games init networking

---
+++ MultiMogul/Hooks/StartingElevatorHooks.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using MultiMogul.MultiMogul.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiMogul.MultiMogul.Hooks
+{
+    [HarmonyPatch]
+    public class StartingElevatorHooks
+    {
+
+        // this patch allows new saves to properly initialize networking
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StartingElevator), "Update")]
+        public static void PostUpdate(StartingElevator __instance)
+        {
+            if (!SavingLoadingManager.Instance.SceneWasLoadedFromNewGame)
+                return;
+
+            FieldInfo _isLowering = typeof(StartingElevator).GetField("_isLowering", BindingFlags.NonPublic | BindingFlags.Instance);
+            if ((bool)_isLowering.GetValue(__instance))
+                return;
+
+            string saveFile = SaveManager.SaveGame(SavingLoadingManager.Instance.ActiveSaveFileName, true);
+            SaveManager.LoadGame(SavingLoadingManager.Instance, saveFile, true);
+
+            MultiMogulBase.serverManager.StartServer();
+            SavingLoadingManager.Instance.SceneWasLoadedFromNewGame = false;
+        }
+    }
+}
