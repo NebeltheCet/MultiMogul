63ba9476e3befd44ca085b06c764bb0d47568702
TheRealBertrand <160984049+TheRealBertrand@users.noreply.github.com>
2025-12-29 12:12:51 -0500

    Added networking for the dropping of items

---
+++ MultiMogul/Hooks/BaseHeldToolHooks.cs
@@ -0,0 +1,218 @@
+﻿using HarmonyLib;
+using MultiMogul.MultiMogul.Utilities;
+using Steamworks.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace MultiMogul.MultiMogul.Hooks
+{
+    [HarmonyPatch]
+    public class BaseHeldToolHooks
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BaseHeldTool), "TryAddToInventory")]
+        public static bool PreTryAddToInventory(BaseHeldTool __instance, int slotIndex = -1)
+        {
+            int GUID = NetworkedObjectRegistry.GetGUIDHashFromInstance(__instance);
+
+            if (UnityEngine.Object.FindObjectOfType<PlayerInventory>().TryAddToInventory(__instance, slotIndex))
+            {
+                Debug.Log("TryAddToInventory Ran");
+
+                if (!ClientManager.IsHost())
+                {
+                    using (Packet packet = new Packet(PacketType.OnRPCMessage))
+                    {
+                        packet.Write("SV_ItemRemoved");
+                        packet.Write(GUID);
+                        packet.Write((int)__instance.SavableObjectID);
+
+                        packet.Send(ClientManager.Instance.connection.Connection, SendType.Reliable);
+                    }
+                }
+                else
+                {
+                    foreach (var kvp in MultiMogulBase.serverManager.connectedClients)
+                    {
+                        using (Packet packet = new Packet(PacketType.OnRPCMessage))
+                        {
+                            packet.Write("CL_ItemRemoved");
+                            packet.Write(GUID);
+                            packet.Write((int)__instance.SavableObjectID);
+
+                            packet.Send(kvp.Key, SendType.Reliable);
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        [Networkable("SV_ItemRemoved")]
+        public static void ItemRemovedServer(Connection connection, Packet receivedPacket)
+        {
+            int GUIDHash = receivedPacket.ReadInt32();
+            GameObject obj = NetworkedObjectRegistry.GetFromGUID(GUIDHash) as GameObject;
+            UnityEngine.Object.Destroy(obj);
+
+            Debug.Log("SV_ItemRemoved Ran");
+
+            foreach (var kvp in MultiMogulBase.serverManager.connectedClients)
+            {
+                if (kvp.Key == connection)
+                    continue;
+
+                using (Packet packet = new Packet(PacketType.OnRPCMessage))
+                {
+                    packet.Write("CL_ItemRemoved");
+                    packet.Write(GUIDHash);
+
+                    packet.Send(kvp.Key, SendType.Reliable);
+                }
+            }
+
+            receivedPacket.Dispose();
+        }
+
+        [Networkable("CL_ItemRemoved")]
+        public static void ItemRemoved(Packet receivedPacket)
+        {
+            int GUIDHash = receivedPacket.ReadInt32();
+            GameObject obj = NetworkedObjectRegistry.GetFromGUID(GUIDHash) as GameObject;
+            UnityEngine.Object.Destroy(obj);
+
+            Debug.Log("CL_ItemRemoved Ran");
+
+            receivedPacket.Dispose();
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BaseHeldTool), "DropItem")]
+        public static bool PreDropItem(BaseHeldTool __instance)
+        {
+            __instance.gameObject.SetActive(true);
+            UnityEngine.Object.FindObjectOfType<PlayerInventory>().RemoveFromInventory(__instance, 1);
+            __instance.HideWorldModel(false);
+            __instance.HideViewModel(true);
+            Rigidbody componentInChildren = __instance.GetComponentInChildren<Rigidbody>();
+            if (componentInChildren != null)
+            {
+                __instance.transform.parent = null;
+                Transform transform = UnityEngine.Object.FindObjectOfType<PlayerController>().PlayerCamera.transform;
+                componentInChildren.isKinematic = false;
+                componentInChildren.transform.position = transform.position + transform.forward * 0.5f;
+                componentInChildren.position = transform.position + transform.forward * 0.5f;
+                componentInChildren.linearVelocity = transform.forward * 5f;
+                componentInChildren.rotation = transform.rotation;
+            }
+            __instance.Owner = null;
+
+            NetworkedObjectRegistry.Register(__instance);
+
+            if (!ClientManager.IsHost())
+            {
+                using (Packet packet = new Packet(PacketType.OnRPCMessage))
+                {
+                    packet.Write("SV_ItemDropped");
+                    packet.Write(NetworkedObjectRegistry.GetGUIDFromInstance(__instance));
+                    packet.Write((int)__instance.SavableObjectID);
+                    packet.Write(componentInChildren.transform.position);
+                    packet.Write(componentInChildren.rotation.eulerAngles);
+                    packet.Write(componentInChildren.linearVelocity);
+
+                    packet.Send(ClientManager.Instance.connection.Connection, SendType.Reliable);
+                }
+            }
+            else
+            {
+                foreach (var kvp in MultiMogulBase.serverManager.connectedClients)
+                {
+                    using (Packet packet = new Packet(PacketType.OnRPCMessage))
+                    {
+                        packet.Write("CL_ItemDropped");
+                        packet.Write(NetworkedObjectRegistry.GetGUIDFromInstance(__instance));
+                        packet.Write((int)__instance.SavableObjectID);
+                        packet.Write(componentInChildren.transform.position);
+                        packet.Write(componentInChildren.rotation.eulerAngles);
+                        packet.Write(componentInChildren.linearVelocity);
+
+                        packet.Send(kvp.Key, SendType.Reliable);
+                    }
+                }
+            }
+
+            Debug.Log("PreDropItem");
+
+            return false;
+        }
+
+        [Networkable("SV_ItemDropped")]
+        public static void ItemDroppedServer(Connection connection, Packet receivedPacket)
+        {
+            string GUID = receivedPacket.ReadString();
+            int savedObjectID = receivedPacket.ReadInt32();
+            Vector3 position = receivedPacket.ReadVector3();
+            Vector3 rotation = receivedPacket.ReadVector3();
+            Vector3 linearVelocity = receivedPacket.ReadVector3();
+
+            UnityEngine.Object prefab = SavingLoadingManager.Instance.GetPrefab((SavableObjectID)savedObjectID);
+
+            UnityEngine.Object __instance = UnityEngine.Object.Instantiate(prefab, position, Quaternion.identity);
+            NetworkedObjectRegistry.Register(__instance, GUID);
+            Rigidbody componentInChildren = __instance.GetComponentInChildren<Rigidbody>();
+            if (componentInChildren != null)
+            {
+                componentInChildren.rotation = Quaternion.Euler(rotation);
+                componentInChildren.linearVelocity = linearVelocity;
+            }
+
+            foreach (var kvp in MultiMogulBase.serverManager.connectedClients)
+            {
+                if (kvp.Key == connection)
+                    continue;
+
+                using (Packet packet = new Packet(PacketType.OnRPCMessage))
+                {
+                    packet.Write("CL_ItemDropped");
+                    packet.Write(NetworkedObjectRegistry.GetGUIDFromInstance(__instance));
+                    packet.Write(savedObjectID);
+                    packet.Write(position);
+
+                    packet.Send(kvp.Key, SendType.Reliable);
+                }
+            }
+
+            receivedPacket.Dispose();
+        }
+
+        [Networkable("CL_ItemDropped")]
+        public static void ItemDropped(Packet receivedPacket)
+        {
+            string GUID = receivedPacket.ReadString();
+            int savedObjectID = receivedPacket.ReadInt32();
+            Vector3 position = receivedPacket.ReadVector3();
+            Vector3 rotation = receivedPacket.ReadVector3();
+            Vector3 linearVelocity = receivedPacket.ReadVector3();
+
+            UnityEngine.Object prefab = SavingLoadingManager.Instance.GetPrefab((SavableObjectID)savedObjectID);
+
+            UnityEngine.Object __instance = UnityEngine.Object.Instantiate(prefab, position, Quaternion.identity);
+            NetworkedObjectRegistry.Register(__instance, GUID);
+            Rigidbody componentInChildren = __instance.GetComponentInChildren<Rigidbody>();
+            if (componentInChildren != null)
+            {
+                componentInChildren.rotation = Quaternion.Euler(rotation);
+                componentInChildren.linearVelocity = linearVelocity;
+            }
+
+            receivedPacket.Dispose();
+        }
+    }
+}
--- MultiMogulBase.cs
+++ MultiMogulBase.cs
@@ -10,7 +10,6 @@ namespace MultiMogul;
 TODO:
 
 keep track of separate inventories per player
-Dropping of Items
 register guid's for ingame instantiated objects and network them
 
 Bonus TODO:
@@ -21,6 +20,7 @@ quest ui jitters when receiving a quest update
 pickup quests dont get updated when the client does them
 older save files are broken on load, implement alternative save version for multiplayer and let it convert the old save files to our own structure
 items picked up in any way have a chance to have their entire physics desynced for other clients, these items have a chance to fall through the map, could also cause nullrefs
+tools picked up with duped id's cause deletion of the player who dropped its inventory getting deleted
 */
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
