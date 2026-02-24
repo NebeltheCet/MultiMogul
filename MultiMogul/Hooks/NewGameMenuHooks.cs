119cfa7aebf380ffa229b8541d3dcdebae1b3e25
NebeltheCet <109384077+NebeltheCet@users.noreply.github.com>
2026-02-24 02:53:03 +0100

    cleaned up UI Helper

--- MultiMogul/Hooks/NewGameMenuHooks.cs
+++ MultiMogul/Hooks/NewGameMenuHooks.cs
@@ -1,14 +1,9 @@
 ﻿using HarmonyLib;
 using MultiMogul.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static TMPro.TMP_InputField;
 
 namespace MultiMogul.Hooks;
 
@@ -23,14 +18,17 @@ public class NewGameMenuHooks {
 	public static void PostOnEnable(NewGameMenu __instance) {
 		ReflectionField<TMP_InputField> newSaveFileNameInputField = ReflectionHelper.GetField<TMP_InputField>(__instance, "_newSaveFileNameInputField", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
-		if (!nameInput) 
-			nameInput = UIHelper.CreateInputFieldFromInstance(newSaveFileNameInputField, "LobbyNameTextInput", new Vector2(-300, -30), "", "Lobby name: 'MMTestLobby'", false);
+		if (!nameInput) {
+			nameInput = UIHelper.CreateInputFieldFromInstance(newSaveFileNameInputField, "LobbyNameTextInput", new Vector2(-300, -30), "", "Lobby name: 'MMTestLobby'");
+		}
 
-		if (!passwordInput) 
-			passwordInput = UIHelper.CreateInputFieldFromInstance(newSaveFileNameInputField, "LobbyPasswordTextInput", new Vector2(-300, -70), "", "Lobby password: '123'", false);
+		if (!passwordInput) {
+			passwordInput = UIHelper.CreateInputFieldFromInstance(newSaveFileNameInputField, "LobbyPasswordTextInput", new Vector2(-300, -70), "", "Lobby password: '123'", ContentType.Password);
+		}
 
-		if (!maxPlayerInput) 
-			maxPlayerInput = UIHelper.CreateInputFieldFromInstance(newSaveFileNameInputField, "LobbyMaxPlayersTextInput", new Vector2(-300, -110), "", "Lobby max players: '8'", true);
+		if (!maxPlayerInput) {
+			maxPlayerInput = UIHelper.CreateInputFieldFromInstance(newSaveFileNameInputField, "LobbyMaxPlayersTextInput", new Vector2(-300, -110), "", "Lobby max players: '8'", ContentType.IntegerNumber);
+		}
 
 		nameInput.text = "";
 		passwordInput.text = "";
@@ -42,7 +40,6 @@ public class NewGameMenuHooks {
 	[HarmonyPatch(typeof(NewGameMenu), "Update")]
 	public static void PostUpdate(NewGameMenu __instance) {
 		ReflectionField<Button> confirmNewGameButton = ReflectionHelper.GetField<Button>(__instance, "_confirmNewGameButton", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-
 		if (string.IsNullOrEmpty(nameInput.text) || string.IsNullOrEmpty(maxPlayerInput.text)) {
 			confirmNewGameButton.Value.interactable = false;
 			confirmNewGameButton.Value.enabled = false;
--- MultiMogul/Utilities/UIHelper.cs
+++ MultiMogul/Utilities/UIHelper.cs
@@ -7,25 +7,29 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using static TMPro.TMP_InputField;
 
 namespace MultiMogul.Utilities;
 
 public class UIHelper {
-	public static TMP_InputField CreateInputFieldFromInstance(TMP_InputField instance, string name, UnityEngine.Vector2 pos, string text, string hint, bool numbersOnly) {
+	public static TMP_InputField CreateInputFieldFromInstance(TMP_InputField instance, string name, UnityEngine.Vector2 pos, string text, string hint, ContentType contentType = ContentType.Standard) {
 		TMP_InputField newInput = UnityEngine.Object.Instantiate(instance, instance.transform.parent);
 		newInput.name = name;
 		newInput.text = text;
 
 		RectTransform rectTransform = newInput.GetComponent<RectTransform>();
-		if(rectTransform != null)
+		if (rectTransform != null) {
 			rectTransform.anchoredPosition = pos;
+		}
 		
 		TMP_Text placeholder = newInput.placeholder as TMP_Text;
-		if(placeholder != null) 
+		if (placeholder != null) {
 			placeholder.text = hint;
-		
-		if (numbersOnly)
-			newInput.contentType = TMP_InputField.ContentType.IntegerNumber;
+		}
+
+		if (contentType != ContentType.Standard) {
+			newInput.contentType = contentType;
+		}
 
 		return newInput;
 	}
